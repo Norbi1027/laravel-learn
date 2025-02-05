ae34c37cc8d3813ef7ac812f97f1f5b97f47e2b9
Norbi1027 <sandornorbi7@gmail.com>
2025-02-05 16:31:31 +0100

    elso

---
+++ resources/views/about.blade.php
@@ -0,0 +1,11 @@
+@extends('layout.layout')
+
+@section('title')
+    About
+@endsection
+
+@section('content')
+<div class="container mt-4">
+<h1>about</h1>
+</div>
+@endsection
---
+++ resources/views/home.blade.php
@@ -0,0 +1,12 @@
+@extends('layout.layout')
+
+@section('title')
+    home
+@endsection
+
+@section('content')
+<div class="container mt-4">
+    <h1>Welcome to my page</h1>
+</div>
+    
+@endsection
